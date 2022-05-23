88cffb099d28407edbea690e2059cb0f0a29792e
Paulo Godoy <paulo.godoy@forefront.se>
2022-05-23 16:13:05 +0200

    Add git

--- powershell/customer-02.ps1
+++ powershell/customer-02.ps1
@@ -1,4 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install git
 choco install notepadplusplus -y
 choco install googlechrome -y
 choco install vscode -y
